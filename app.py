7c43103bf3b3ddb6121b050c6dd23a406577cc5c
ᴡᴏᴊᴄɪᴇᴄʜ ɢʀᴢʏʙᴏᴡꜱᴋɪ <wojgrz5@st.amu.edu.pl>
2023-08-04 18:17:43 +0200

    Finished login and sign-up logic

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from flask import Flask, render_template, request, Response, redirect, make_response
 import mysql.connector
 
@@ -26,7 +28,7 @@ def sign_up():
 
 
 @app.route("/api/search_login", methods=["POST"])
-def check_login():
+def login_api():
     try:
         host = "localhost"
         user = "root"
@@ -44,6 +46,33 @@ def check_login():
         response.set_cookie("login", user_login)
         response.set_cookie("note", "", expires=0)
         return response
+    except:
+        return Response(
+            response=f"Cannot connect to database is server running? are the credentials correct? is database created?",
+            status=500)
+
+
+@app.route("/api/sign_up", methods=["POST"])
+def signup_api():
+    login = request.form.get("login")
+    password = request.form.get("password")
+    hashing = hashlib.sha3_256()
+    hashing.update(password.encode("utf8"))
+    hashed_password = hashing.hexdigest()
+    try:
+        host = "localhost"
+        user = "root"
+        password = ""
+        database = "sklep"
+        connect = mysql.connector.connect(host=host, user=user, password=password, database=database)
+        cursor = connect.cursor()
+        query = f"INSERT INTO users VALUES ('', '{login}', '{hashed_password}');"
+        cursor.execute(query)
+        connect.commit()
+        connect.close()
+        resp = redirect("/")
+        resp.set_cookie("login", login)
+        return resp
     except:
         return Response(
             response=f"Cannot connect to database is server running? are the credentials correct? is database created?",
--- static/script/script.js
+++ static/script/script.js
@@ -1,38 +1,66 @@
 let timer;
 
 function is_login_available(login) {
-  if (login === "") {
-    document.getElementById("login_not_available").style.display = "none";
-    document.getElementById("login_available").style.display = "none";
-    return;
-  }
-
-  clearTimeout(timer);
-
-  timer = setTimeout(function () {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:5000/api/is_login_available", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-      "login": login
-    }));
-    xhr.onload = function () {
-      if (login === "") {
+    if (login === "") {
         document.getElementById("login_not_available").style.display = "none";
         document.getElementById("login_available").style.display = "none";
+        document.getElementById("submit").classList.add("not-allowed")
         return;
-      }
-      if (this.status === 200) {
-        login_not_available = document.getElementById("login_not_available").style.display = "none";
-        login_available = document.getElementById("login_available");
-        login_available.innerHTML = "✔ login <i>" + login + "</i> jest dostępny";
-        login_available.style.display = "block";
-      } else {
-        login_available = document.getElementById("login_available").style.display = "none";
-        login_not_available = document.getElementById("login_not_available");
-        login_not_available.innerHTML = "❌ login <i>" + login + "</i> jest już zajęty";
-        login_not_available.style.display = "block";
-      }
-    };
-  }, 250)
+    }
+
+    clearTimeout(timer);
+
+    timer = setTimeout(function () {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:5000/api/is_login_available", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            "login": login
+        }));
+        xhr.onload = function () {
+            if (login === "") {
+                document.getElementById("login_not_available").style.display = "none";
+                document.getElementById("login_available").style.display = "none";
+                document.getElementById("submit").style.backgroundColor = "grey"
+                return;
+            }
+            if (this.status === 200) {
+                login_not_available = document.getElementById("login_not_available").style.display = "none";
+                login_available = document.getElementById("login_available");
+                login_available.innerHTML = "✔ login <i>" + login + "</i> jest dostępny";
+                login_available.style.display = "block";
+                document.getElementById("submit").removeAttribute("disabled")
+                document.getElementById("submit").classList.remove("not-allowed")
+                document.getElementById("submit").classList.add("allowed")
+            } else {
+                login_available = document.getElementById("login_available").style.display = "none";
+                login_not_available = document.getElementById("login_not_available");
+                login_not_available.innerHTML = "❌ login <i>" + login + "</i> jest już zajęty";
+                login_not_available.style.display = "block";
+                document.getElementById("submit").setAttribute("disabled", "yes")
+                document.getElementById("submit").classList.remove("allowed")
+                document.getElementById("submit").classList.add("not-allowed")
+            }
+        };
+    }, 250)
+}
+
+function check_form() {
+    let login = document.getElementById("login")
+    if (login.value === "") {
+        return false
+    }
+    let password = document.getElementById("password")
+    let rpt_password = document.getElementById("rpt_password")
+    if (password.value !== rpt_password.value) {
+        document.getElementById("passwords_not_identical").style.display = "block"
+        password.classList.add("bad")
+        rpt_password.classList.add("bad")
+        return false
+    } else {
+        password.classList.remove("not-identical")
+        rpt_password.classList.remove("not-identical")
+        return true
+    }
+
 }
