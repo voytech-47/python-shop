cdb73ab6ca0c87934ac2b6b95e53e4b578997dd0
ᴡᴏᴊᴄɪᴇᴄʜ ɢʀᴢʏʙᴏᴡꜱᴋɪ <wojgrz5@st.amu.edu.pl>
2023-08-28 13:06:40 +0200

    reorganised file structure

--- static/script/script.js
+++ static/script/script.js
@@ -1,14 +1,11 @@
 function is_login_available(login) {
-    let timer;
     if (login === "") {
         document.getElementById("login_not_available").style.display = "none";
         document.getElementById("login_available").style.display = "none"
         return;
     }
 
-    clearTimeout(timer);
-
-    timer = setTimeout(function () {
+    setTimeout(function () {
         const xhr = new XMLHttpRequest();
         xhr.open("POST", "http://localhost:5000/api/is_login_available", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -21,8 +18,10 @@ function is_login_available(login) {
                 document.getElementById("login_available").style.display = "none";
                 return;
             }
+            let login_not_available;
+            let login_available;
             if (this.status === 200) {
-                login_not_available = document.getElementById("login_not_available").style.display = "none";
+                document.getElementById("login_not_available").style.display = "none";
                 login_available = document.getElementById("login_available");
                 login_available.innerHTML = "✔ login <i>" + login + "</i> jest dostępny";
                 login_available.style.display = "block";
@@ -30,7 +29,7 @@ function is_login_available(login) {
                 document.getElementById("submit").classList.remove("not-allowed")
                 document.getElementById("submit").classList.add("allowed")
             } else {
-                login_available = document.getElementById("login_available").style.display = "none";
+                document.getElementById("login_available").style.display = "none";
                 login_not_available = document.getElementById("login_not_available");
                 login_not_available.innerHTML = "❌ login <i>" + login + "</i> jest już zajęty";
                 login_not_available.style.display = "block";
@@ -62,30 +61,8 @@ function check_form() {
 
 }
 
-function add_to_cart(raw) {
-    let current_cart_value = get_cookie("cart_value")
-    if (current_cart_value === "") {
-        current_cart_value = 0.0
-    } else {
-        current_cart_value = parseFloat(current_cart_value)
-    }
-    let current_cart_items = get_cookie("cart_contents")
-    let item = raw.getAttribute("data").slice(1, -1).split(", ")
-    let item_value = parseFloat(item[2])
-    let item_id = item[0] + "_"
-    current_cart_value += item_value
-    current_cart_items += item_id
-    set_cookie("cart_value", current_cart_value.toFixed(2))
-    set_cookie("cart_contents", current_cart_items)
-    let tooltip = document.getElementsByClassName("tooltip " + item[0])[0]
-    tooltip.classList.add("active");
-    setTimeout(() => {
-        tooltip.classList.remove("active");
-    }, 2000);
-}
-
 function chech_if_changed() {
-    var url = new URL(window.location.href);
+    let url = new URL(window.location.href);
     let last_changed = url.searchParams.get("last_changed")
     let tooltip = document.getElementsByClassName("tooltip " + last_changed)[0]
     tooltip.classList.add("active")
@@ -99,20 +76,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.remove("preload")
     chech_if_changed()
 });
-
-function get_cookie(name) {
-    return ('; ' + document.cookie).split(`; ` + name + `=`).pop().split(';')[0];
-}
-
-function set_cookie(name, value) {
-    document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; path=/`;
-}
-
-function change_amount(field) {
-    if (field.value === 0) {
-        field.value = 1
-    }
-}
 
 function update_amount(field) {
     let amount = parseInt(field.value);
@@ -136,79 +99,20 @@ function update_amount_on_server(field, id, amount) {
             "Content-Type": "multipart/form-data",
         },
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-    })
-    .then(data => {
-        console.log("Response data:", data);
-        location.reload();
-    })
-    .catch(error => {
-        console.error("Fetch error:", error);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+        })
+        .then(data => {
+            console.log("Response data:", data);
+            location.reload();
+        })
+        .catch(error => {
+            console.error("Fetch error:", error);
+        });
 }
-
-
-function update_amount_click(field, value) {
-    let id = field.classList[1]
-    let price = parseFloat(document.getElementsByClassName("amount " + id)[0].getAttribute("data"))
-    let input_value = parseInt(document.getElementsByClassName("amount " + id)[0].value)
-    if (input_value === 1 && value === -1) {
-        return
-    } else {
-        document.getElementsByClassName("minus-wrap " + id)[0].classList.remove("disabled")
-    }
-    input_value += parseInt(value)
-    document.getElementsByClassName("amount " + field.classList[1])[0].value = input_value
-    document.getElementsByClassName("amount " + field.classList[1])[0].setAttribute("old_value", input_value)
-    document.getElementsByClassName("price " + id)[0].innerHTML = (price * input_value).toFixed(2) + " zł (" + input_value + " * " + price + " zł)"
-    let cart_value = parseFloat(document.getElementById("sum").getAttribute("data"))
-    if (value > 0) {
-        cart_value += price
-    } else {
-        cart_value -= price
-    }
-    set_cookie("cart_value", cart_value.toFixed(2))
-    set_cart(cart_value)
-    if (input_value === 1) {
-        document.getElementsByClassName("minus-wrap " + id)[0].classList.add("disabled")
-    }
-}
-
-function delete_item_from_cart(field) {
-    let cart_value = parseFloat(document.getElementById("sum").getAttribute("data"))
-    let id = field.classList[1]
-    let item = document.getElementsByClassName("amount " + id)[0]
-    let amount = item.value
-    let price = item.getAttribute("data")
-    let substr = (amount * price).toFixed(2)
-    cart_value -= parseFloat(substr)
-    set_cart(cart_value)
-    set_cookie("cart_value", cart_value.toFixed(2))
-    let old_items = get_cookie("cart_contents")
-    let new_items = old_items.split(id + "_").join("")
-    set_cookie("cart_contents", new_items)
-    document.getElementsByClassName("item " + id)[0].remove()
-    if (get_cookie("cart_contents") === "") {
-        document.getElementById("cart_empty").style.display = "block"
-    }
-
-}
-
 
 function confirm_change(prompt) {
     return confirm(prompt)
-}
-
-function set_cart(value) {
-    document.getElementById("sum").innerHTML = "Suma: " + value.toFixed(2) + " zł"
-    document.getElementById("sum").setAttribute("data", value.toFixed(2))
-    document.getElementById("cart_value").innerHTML = value.toFixed(2) + " zł"
-}
-
-function show_ses() {
-    console.log(session)
-    console.log(session["cart"])
 }
